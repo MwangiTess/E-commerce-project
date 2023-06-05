bcc41bdf8598b2b1264ffeb525be0a3d96525516
Ryan <ryanbiy01@gmail.com>
2023-06-05 14:46:55 +0300

    Added search box

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,26 @@
-const  Home= () => {
-    return ( 
-        <div>
-            <p>This is my home page</p>
-        </div>
-     );
-}
- 
+import React, { useState } from 'react';
+
+const Home = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  return (
+    <div>
+      <p>This is my home page</p>
+      <div>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleSearch}
+          placeholder="Search..."
+        />
+        <button onClick={() => console.log('Search:', searchTerm)}>Search</button>
+      </div>
+    </div>
+  );
+};
+
 export default Home;
