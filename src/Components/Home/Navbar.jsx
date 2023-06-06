423738f210cd745487e47fd26901ed434b4ce4df
Ryan <ryanbiy01@gmail.com>
2023-06-06 14:43:17 +0300

    Added dropdown menu for account button

--- src/Components/Home/Navbar.jsx
+++ src/Components/Home/Navbar.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   return (
     <nav className="navbar">
       <div className="logo-container">
-        <Link to="/" className="logo">Wetto Shop</Link>
+        <Link to="/" className="logo">
+          Wetto Shop
+        </Link>
       </div>
       <div className="options-container">
         <ul className="options">
@@ -29,7 +37,19 @@ const Navbar = () => {
         <button className="search-button">Search</button>
       </div>
       <div className="actions-container">
-        <button className="account-button">Account</button>
+        <button className="account-button" onClick={toggleDropdown}>
+          Account
+        </button>
+        {showDropdown && (
+          <ul className="dropdown-menu">
+            <li>
+              <Link to="/signin">Sign In</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        )}
         <Link to="/cart" className="cart-button">
           Cart
         </Link>
@@ -37,4 +57,5 @@ const Navbar = () => {
     </nav>
   );
 };
+
 export default Navbar;
