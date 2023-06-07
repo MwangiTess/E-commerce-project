cc3212e0b9176c28e89b493d51a4f45cffc6fa40
Ryan <ryanbiy01@gmail.com>
2023-06-07 08:30:26 +0300

    Revert "Added footer"
    This reverts commit d6f28ec5c3e6f3b9a604f57420fbc78c6314547f.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from '../Home/Navbar.jsx';
-import Footer from '../Home/Footer.jsx';
 import Home from '../LandingPage/Home.jsx';
 import Products from '../LandingPage/Products.jsx';
 import ShoppingCart from '../Cart/shopping_cart.jsx';
@@ -13,22 +12,20 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter>
-      <div className="app-container">
+      <div>
         <Navbar />
-        <div className="content-container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<ShoppingCart />} />
-            <Route path="/products/*" element={<Products />} />
-            <Route path="/products/:id" element={<Product />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signin" element={<SignUp />} />
-          </Routes>
-        </div>
-        <Footer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<ShoppingCart />} />
+          <Route path="/products/*" element={<Products />} />
+          <Route path="/products/:id" element={<Product />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signin' element={<SignUp />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
 }
 
 export default App;
+
