a57d0480ab1840052500a3e034d6b4f5a81c1a7c
Yegon Japheth Kiptoo <jyegon504@gmail.com>
2023-06-08 13:29:37 +0300

    EC-6 Done Login-Signin btn responsivity

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './SignUp.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 function SignUp(){
+
+    const navigate = useNavigate();
+    const [loginData, setLoginData] = useState({
+    name: '',
+    email: '',
+    contact: '',
+    password: '',
+  })
+
+  const handleChange = (e) => {
+    setLoginData({...loginData, [e.target.name]: e.target.value})
+  };
+
+  console.log(loginData);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate('/')
+  };
 
     return (
         <div className='Layout'>
@@ -10,40 +29,44 @@ function SignUp(){
                 <h1 id="shopName">WETTO SHOP</h1>
                 <h1 id="createAccount">Create an account</h1>
                 <h3 id="connect">Connect with your friends today!</h3>
-                <div id="signUpBox">
+                <form id="signUpBox" onChange={handleChange} onSubmit={handleSubmit}>
                     <input
                      type= "text"
-                     name= "username"
+                     name= "name"
                      placeholder= "Enter Your Username"
                      className='inputs'
+                     required
                      /><br/>
                      <input
                      type= "email"
-                     name= "useremail"
+                     name= "email"
                      placeholder= "Enter Your Email"
                      className='inputs'
+                     required
                      /><br/>
                      <input
                      type= "tel"
-                     name= "usercontact"
+                     name= "contact"
                      placeholder= "Enter Your Phone Number"
                      className='inputs'
+                     required
                      /><br/>
                      <input
                      type= "password"
                      name= "password"
                      placeholder= "Enter Your Password"
                      className='inputs'
+                     required
                      /><br/>
                      <button id='s-btn'>
                         Sign Up
                      </button>
-                </div>
+                </form>
                 <p id='account'>
                     Already have an account? < NavLink to = "/login">Login</NavLink>
                 </p>
             </div>
-            <img className='image' src="/images/cart.jpg" alt="cart"/>
+            <img className='s-image' src="/images/cart.jpg" alt="cart"/>
         </div>
     );
 }
